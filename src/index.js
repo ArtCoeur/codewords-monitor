520eb39b798a2274b8a029b42d70d83d4fb05d12
Tim Rodger <tim.rodger@gmail.com>
2015-04-10 23:04:04 +0100

    Commits basic rabbit mq handler code

---
+++ src/index.js
@@ -0,0 +1,39 @@
+var logger = require('./lib/logger'),
+    pattern = require('./lib/pattern'),
+    router = require('./lib/router'),
+    rabbitmq = require('rabbit.js');
+
+// wait until rabbitmq can accept connections, somehow
+function doConnect(){
+
+    try {
+        var context = rabbitmq.createContext(
+            'amqp://' + process.env.RABBITMQ_PORT_5672_TCP_ADDR + ':' + process.env.RABBITMQ_PORT_5672_TCP_PORT
+        );
+
+        context.on('ready', function() {
+
+            logger.info('monitor: connected');
+
+            // subscribe to events queues
+            var sub = context.socket('SUB'),
+                pub = context.socket('PUB');
+
+            sub.connect('events', function () {
+
+                // deal with facts as they come in
+                sub.on('data', function (body) {
+                    logger.info('monitor : ' + body);
+                    router.newFact(pub, JSON.parse(body));
+                });
+            });
+        });
+
+    } catch (err){
+        logger.error("caught error trying to connect to rabbitmq" + err);
+        setTimeout(doConnect, 2000);
+    }
+}
+
+// hack to wait till rabbitmq is up
+setTimeout(doConnect, 12000);
---
+++ src/lib/router.js
@@ -0,0 +1,18 @@
+/**
+ * Handle all facts
+ *
+ * @param fact
+ */
+module.exports.newFact = function(pub, fact) {
+    switch (fact.name){
+        case 'board.new':
+            break;
+        case 'word.new':
+            break;
+        case 'cell.updated':
+            break;
+        default :
+            //
+
+    }
+};
