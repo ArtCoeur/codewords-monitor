93ede0d0340606ed919498bd7f168f038f2e5381
Tim Rodger <tim.rodger@gmail.com>
2015-05-09 16:10:08 +0100

    Uses forever to run app

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
 var logger = require('./lib/logger'),
-    router = require('./lib/router'),
-    rabbitmq = require('rabbit.js');
+    rabbitmq = require('rabbit.js'),
+    router = require('./lib/router');
 
-// wait until rabbitmq can accept connections, somehow
-function doConnect(){
+logger.info('running');
 
-    try {
-        var context = rabbitmq.createContext(
-            'amqp://' + process.env.RABBITMQ_PORT_5672_TCP_ADDR + ':' + process.env.RABBITMQ_PORT_5672_TCP_PORT
-        );
+var context = rabbitmq.createContext(
+    'amqp://' + process.env.RABBITMQ_PORT_5672_TCP_ADDR + ':' + process.env.RABBITMQ_PORT_5672_TCP_PORT
+);
 
-        context.on('ready', function() {
+context.on('ready', function() {
 
-            logger.info('monitor: connected');
+    logger.info('connected');
 
-            // subscribe to events queues
-            var sub = context.socket('SUB'),
-                pub = context.socket('PUB');
+    var sub = context.socket('SUB'),
+        pub = context.socket('PUB');
 
-            pub.connect('events', function() {
-                sub.connect('events', function () {
+    pub.connect('events', function() {
+        sub.connect('events', function () {
 
-                    // deal with facts as they come in
-                    sub.on('data', function (body) {
-                        router.newFact(pub, JSON.parse(body));
-                    });
-                });
+            // deal with facts as they come in
+            sub.on('data', function (body) {
+                router.newFact(pub, JSON.parse(body));
             });
         });
-
-    } catch (err){
-        logger.error("caught error trying to connect to rabbitmq" + err);
-        setTimeout(doConnect, 2000);
-    }
-}
-
-// hack to wait till rabbitmq is up
-setTimeout(doConnect, 12000);
+    });
+});
